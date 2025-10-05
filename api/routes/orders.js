42ef5a9183ce2bf6607908800263f30ceda532b4
Tiiso Senosha <theotiiso@gmail.com>
2025-10-05 23:18:50 +0200

    Added routes

---
+++ api/routes/orders.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order was created'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    const id =req.params.productId;
+    res.status(200).json({
+        message: 'order details',
+        id: id
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'order deleted',
+    });
+});
+
+module.exports = router;
